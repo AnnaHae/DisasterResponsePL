ae13d08bb7db4a22bdcac604d2bc8e8ade338319
Anna Haenchen <annatrumm@aol.com>
2020-09-06 14:18:54 +0200

    new py-files

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,58 @@
 import sys
-
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    ''' loads datasets and merges them
+        returns: dataframe'''
+    
+    #load message filepath
+    message=pd.read_csv(message_filepath)
+    
+    # load categories filepath
+    categories=pd.read_csv(categories_filepath)
+   
+    #merge both datasets
+    df=pd.merge(messages,categories, on='id')
 
+    return df
 
 def clean_data(df):
-    pass
-
+    ''' cleans dataframe
+        returns: dataframe'''
+   
+    # create a dataframe of the 36 individual category columns
+    categories = df.categories.str.split(';',expand=True)
+    # select the first row 
+    row = categories.iloc[0]
+    # use this row to extract a list of new column names for categories.
+    category_colnames = [x[:-2] for x in row]
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    
+    for column in categories:
+    # set each value to be the last character of the string
+        categories[column]= categories[column].str.strip().str[-1]   
+    # convert column from string to numeric
+        categories[column] = categories[column].astype(int) 
+    
+    # drop the original categories column from `df`
+    df.drop(['categories'],axis=1, inplace=True)
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df =  pd.concat([df, categories], axis=1)
+    
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+    
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    ''' saves the data into sqlite database'''
+    
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df.to_sql('messages', engine, index=False)
 
 
 def main():
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,77 @@
 import sys
+import nltk
+nltk.download(['punkt', 'wordnet'])
 
+import re
+import numpy as np
+import pandas as pd
+import pickle
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer,TfidfVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+
+from sqlalchemy import create_engine
 
 def load_data(database_filepath):
-    pass
-
+    '''loads dataframe from database and splits it into X and Y
+        returns X,Y, category_names'''
+    
+    #create sqlite engine
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    
+    # read in df from database
+    df = pd.read_sql('SELECT* FROM messages',engine) 
+    
+    #split df into predictor(X) and criterion(Y) variables
+    X = df.loc[:,'message'].values
+    Y = df.iloc[:,4:].values
+    
+    # get list of categories
+    category_names=df.iloc[:,4:].columns
+    
+    return X,Y,category_names
 
 def tokenize(text):
-    pass
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    # build pipeline
+    pipeline = Pipeline([
+    ('tfidf',TfidfVectorizer(tokenizer=tokenize)),
+    ('moc',MultiOutputClassifier(KNeighborsClassifier()))
+    ])    
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    # predict on test data
+    Y_pred=pipeline.predict(X_test)
+    for i in range (Y_test.shape[1]):
+    print('The classification report for {} is \n {}'.format(df.columns[(i+4)],classification_report(Y_test[:,i],Y_pred[:,i])))
+
 
 
 def save_model(model, model_filepath):
-    pass
+    filename = model_filepath
+    pickle.dump(model, open(filename, 'wb'))
 
 
 def main():
